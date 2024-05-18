6bbca5a4e8f14fab340394983a504a9fac0a372d
minseokkim <minseok5027@cau.ac.kr>
2024-05-18 12:55:04 +0900

    feature: update dto

--- BE-feature-basic/src/main/java/com/commitfarm/farm/dto/project/ProjectListRes.java
+++ BE-feature-basic/src/main/java/com/commitfarm/farm/dto/project/ProjectListRes.java
@@ -14,5 +14,4 @@ public class AllProjectRes {
 
     @Schema(description = "메인 화면에서 PL or Dev or Tester 클릭 -> 해당 유저의 모든 프로젝트를 보여줌(진행된 플젝, 완료된 플젝)")
     private List<ProjectRes> projectList;
-
 }
--- BE-feature-basic/src/main/java/com/commitfarm/farm/dto/ticket/TicketRes.java
+++ BE-feature-basic/src/main/java/com/commitfarm/farm/dto/ticket/TicketRes.java
@@ -1,6 +1,7 @@
 package com.commitfarm.farm.dto.ticket;
 
 import com.commitfarm.farm.domain.enumClass.Status;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,12 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SimpleTicketRes {
+    @Schema(description = "티켓 제목")
     private String title;
+
+    @Schema(description = "티켓 마감일")
     private Date endDate;
+
+    @Schema(description = "티켓 상태")
     private Status status;
 }
